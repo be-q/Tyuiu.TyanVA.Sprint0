f598da73c33ef8252476feee4f653e1ff6f80a0f
Владислав Тян <Tyuiu.TyanVA@outlook.com>
2026-09-13 04:29:00 +0500

    45 y.o

---
+++ Tyuiu.TyanVA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.TyanVA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Be-q";
+            int age = 45;
+            Console.WriteLine("Здравствуйте! Меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey(); 
+        }
+    }
+}
