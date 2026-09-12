3dcb4c40c32e1d189578bb60697ef1b5b65fc683
Владислав Тян <Tyuiu.TyanVA@outlook.com>
2026-09-13 04:21:27 +0500

    Task1 added

---
+++ Tyuiu.TyanVA.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TyanVA.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Be-q";
+            int age = 45;
+            Console.WriteLine("Здравствуйте! Меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
