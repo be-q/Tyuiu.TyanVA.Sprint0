4d8cbc1a438d49d15829b01315a1aa1b84fd3bc4
Владислав Тян <Tyuiu.TyanVA@outlook.com>
2026-09-14 17:34:23 +0500

    Task4

---
+++ Tyuiu.TyanVA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿namespace Tyuiu.TyanVA.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        // Пример линейной функции Сумма
+        public static int Addition(int a, int b)
+        {
+            return a + b; 
+        }
+        // Пример линейной структуры разность
+        public static int Subtraction (int a, int b)
+        {
+            return a - b;
+        }
+        // Пример линейной функции Произведение 
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        // Пример линейной структуры Деление
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }  
+    }
+}
---
+++ Tyuiu.TyanVA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.TyanVA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.TyanVA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример линейной структуры 
+            // Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            // Вызов метода разности Subtraction
+            Console.WriteLine(DataService.Subtraction(5, 1));
+            // Вызов метода произведения Multiplication
+            Console.WriteLine(DataService.Multiplication(2, 5));
+            // Вызов метода деления Division
+            Console.WriteLine(DataService.Division(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
