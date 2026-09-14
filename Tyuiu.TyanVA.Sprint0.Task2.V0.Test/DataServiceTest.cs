cffd0c0ada237a7ccd8a9a0e8273249069ed9c7b
Владислав Тян <Tyuiu.TyanVA@outlook.com>
2026-09-14 14:27:17 +0500

    Task2

---
+++ Tyuiu.TyanVA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TyanVA.Sprint0.Task2.V0.Lib; 
+
+namespace Tyuiu.TyanVA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Влад";
+            var res = DataService.GetMessage(name);
+
+            // Называем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Влад", res);
+        }
+    }
+}
---
+++ Tyuiu.TyanVA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.TyanVA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.TyanVA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage
+            //Из библиотеки Tyuiu.TyanVA.Sprint1.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Влад")); 
+            Console.ReadKey();
+        }
+    }
+}
