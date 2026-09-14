3a7cfdac9d2ea59bf7c1dad5634540149ebe1af9
Владислав Тян <Tyuiu.TyanVA@outlook.com>
2026-09-14 18:04:09 +0500

    Task5

---
+++ Tyuiu.TyanVA.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿namespace Tyuiu.TyanVA.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        // Пример линейной функции Сумма
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        // Пример линейной структуры разность
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        // Пример линейной функции Произведение 
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        // Пример линейной структуры Деление
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                    Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                    return -1;
+            }  
+            else
+            {
+                return a / b;
+            }
+        
+        }
+        
+    }
+}
---
+++ Tyuiu.TyanVA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TyanVA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.TyanVA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Пример линейной структуры
+            Console.WriteLine(" A + B = " + DataService.Addition(2, 5));
+            Console.WriteLine(" A - B = " + DataService.Subtraction(10, 5));
+            Console.WriteLine(" A * B = " + DataService.Multiplication(2, 5));
+            // Пример разветвляющейся структуры находится в библиотеке классов в методе Division 
+            Console.WriteLine(" A / B = " + DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
