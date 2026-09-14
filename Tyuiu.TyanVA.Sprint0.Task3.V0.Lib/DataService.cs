8e13524345ed5c9a908f43b92f5cff665337a3f0
Владислав Тян <Tyuiu.TyanVA@outlook.com>
2026-09-14 15:34:47 +0500

    Task3

---
+++ Tyuiu.TyanVA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.TyanVA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        // Реализация метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
